3d38e64376a551e8b79569dc701683f22b695201
anmoln94 <shramol94@gmail.com>
2017-06-12 15:25:35 +0530

    -

---
+++ Nonrepeatingelement.java
@@ -0,0 +1,46 @@
+package CodingCourse;
+
+public class Nonrepeatingelement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr = new int[]{1,2,2,3,3};
+		
+		int low=0;
+		int high = arr.length-1;
+		int mid;
+		boolean found = false;
+		
+		while(low<=high)
+		{
+			mid = (low+high)/2;
+			
+			if(low==high)
+			{
+				System.out.println(arr[mid]);
+				found = true;
+				break;
+			}
+			
+			if(mid%2==0)
+			{
+				if(arr[mid]==arr[mid+1])
+					low = mid+2;
+				else
+					high = mid;
+			}
+			else
+			{
+				if(arr[mid]==arr[mid-1])
+					low = mid+1;
+				else
+					high = mid-1;
+			}
+			
+		}
+		if(found==false)
+			System.out.println("unique element not found");
+	}
+
+}
