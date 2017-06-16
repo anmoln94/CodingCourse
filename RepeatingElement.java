b1c84701451d819d0fb57cd1c501c4d31a96527b
anmoln94 <shramol94@gmail.com>
2017-06-16 13:19:19 +0530

    -

--- RepeatingElement.java
+++ RepeatingElement.java
@@ -3,7 +3,7 @@ package CodingCourse;
 public class RepeatingElement {
 	 public static void main(String[] args) 
 	 {
-		 int[] arr = new int[]{1,1}; //from 1 to n-1
+		 int[] arr = new int[]{1,2,3,3}; //from 1 to n-1
 		 
 		 int low=0;
 		 int high= arr.length-1;
@@ -15,7 +15,7 @@ public class RepeatingElement {
 			 mid = (low+high)/2;
 			 if(arr[mid]!=mid+1)	//only the 2nd repetition has its index misplaced
 			 {
-				 if(arr[mid]==arr[mid-1])
+				 if(mid>0 && arr[mid]==arr[mid-1])
 				 {
 					  System.out.println(arr[mid]);
 					  found = true;
